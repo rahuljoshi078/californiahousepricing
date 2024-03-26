074114a53dbf8cd02d0c127815d54bc1dc96bf30
Rahul Joshi <rahuljoshi078@gmail.com>
2024-03-26 11:55:21 +0530

    Web app is ready

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 app = Flask(__name__) ## Starting point of my app from where the application will run
 
 ## Load the model
-model = pickle.load(open('regmodel.pkl','rb'))
+regmodel = pickle.load(open('regmodel.pkl','rb'))
 scaler = pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/') ## It says I should definitely go to home page
@@ -23,6 +23,16 @@ def predict_api():
     output = regmodel.predict(new_Data)
     print(output[0])
     return jsonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    print(output)
+    return render_template("home.html", prediction_text = "The House price prediction is {}".format(output))
 
 
 if __name__ == '__main__':
